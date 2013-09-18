79876ca4df87591808b15fad5e8c612f7efb7232
Eric Swann <eric@carecommander.com>
2013-09-18 09:58:44 -0500

    Updated the sample app to run some async operations

--- Source/Samples/SisoDb.SampleApp/Program.cs
+++ Source/Samples/SisoDb.SampleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using SisoDb.Dac;
 using SisoDb.MsMemoryCache;
 using SisoDb.Querying;
@@ -19,19 +20,25 @@ namespace SisoDb.SampleApp
         {
             Console.WriteLine("Hi. Goto the Sample-app and open Program.cs and App.config and ensure that you are satisfied with the connection string.");
             Console.ReadKey();
-            return;
+            //return;
 
             //********* SQL2005 ***********
             //var db = "SisoDb.Sql2005".CreateSql2005Db();
+            //********* SQL2005 Async ***********
+            //var db = "SisoDb.Sql2005.Async".CreateSql2005Db();
             //********* SQL2008 ***********
             //var db = "SisoDb.Sql2008".CreateSql2008Db();
+            //********* SQL2008 Async***********
+            //var db = "SisoDb.Sql2008.Async".CreateSql2008Db();
             //********* SQL2012 ***********
             //var db = "SisoDb.Sql2012".CreateSql2012Db();
+            //********* SQL2012 Async***********
+            var db = "SisoDb.Sql2012.Async".CreateSql2012Db();
             //********* SQLCE4 ***********
             //var db = "SisoDb.SqlCe4".CreateSqlCe4Db();
             //********************************************
 
-            //db.EnsureNewDatabase();
+            db.EnsureNewDatabase();
             //db.CacheProvider = new MsMemCacheProvider();
             //db.CacheProvider.EnableFor(typeof(Customer));
             
@@ -40,26 +47,60 @@ namespace SisoDb.SampleApp
             //db.Settings.AllowDynamicSchemaUpdates = false;
 
             //To get rid of warm up in tests as it first syncs schemas etc
-            //db.UpsertStructureSet<Customer>();
+            db.UpsertStructureSet<Customer>();
 
-            //InsertCustomers(1, 100000, db);
+            InsertCustomers(1, 10000, db);
 
-            //ProfilingInserts(db, 10000, 5);
-            //ProfilingQueries(() => FirstOrDefault(db, 500, 550));
-            //ProfilingQueries(() => SingleOrDefault(db, 500, 550));
-            //ProfilingQueries(() => GetAllCustomers(db));
-            //ProfilingQueries(() => GetAllCustomersAsJson(db));
-            //ProfilingQueries(() => GetCustomersViaIndexesTable(db, 500, 550));
-            //ProfilingQueries(() => GetCustomersAsJsonViaIndexesTable(db, 500, 550));
+            ProfileSyncOperations(db);
 
-            //UpsertSp(db, 500, 550);
-            //ProfilingQueries(() => GetCustomersViaSpExp(db, 500, 550));
-            //ProfilingQueries(() => GetCustomersViaSpRaw(db, 500, 550));
+            db.EnsureNewDatabase();
+            db.UpsertStructureSet<Customer>();
 
-            //ProfilingUpdateMany(db, 500, 550);
+            InsertCustomers(1, 10000, db);
 
-            //Console.WriteLine("---- Done ----");
-            //Console.ReadKey();
+            ProfileAsyncOperations(db);
+
+            Console.WriteLine("---- Done ----");
+            Console.ReadKey();
+        }
+
+        private static void ProfileSyncOperations(ISisoDatabase db)
+        {
+            Console.WriteLine("Profiling Sync Operations");
+
+            ProfilingInserts(db, 10000, 5);
+            ProfilingQueries(() => FirstOrDefault(db, 500, 550));
+            ProfilingQueries(() => SingleOrDefault(db, 500, 550));
+            ProfilingQueries(() => GetAllCustomers(db));
+            ProfilingQueries(() => GetAllCustomersAsJson(db));
+            ProfilingQueries(() => GetCustomersViaIndexesTable(db, 500, 550));
+            ProfilingQueries(() => GetCustomersAsJsonViaIndexesTable(db, 500, 550));
+
+            UpsertSp(db, 500, 550);
+            ProfilingQueries(() => GetCustomersViaSpExp(db, 500, 550));
+            ProfilingQueries(() => GetCustomersViaSpRaw(db, 500, 550));
+
+            ProfilingUpdateMany(db, 500, 550);
+            Console.WriteLine();
+        }
+
+        private static void ProfileAsyncOperations(ISisoDatabase db)
+        {
+            Console.WriteLine("Profiling Async Operations");
+            ProfilingInsertsAsync(db, 10000, 5);
+            ProfilingQueries(() => FirstOrDefaultAsync(db, 500, 550));
+            ProfilingQueries(() => SingleOrDefaultAsync(db, 500, 550));
+            ProfilingQueries(() => GetAllCustomersAsync(db));
+            ProfilingQueries(() => GetAllCustomersAsJsonAsync(db));
+            ProfilingQueries(() => GetCustomersViaIndexesTableAsync(db, 500, 550));
+            ProfilingQueries(() => GetCustomersAsJsonViaIndexesTableAsync(db, 500, 550));
+
+            UpsertSp(db, 500, 550);
+            ProfilingQueries(() => GetCustomersViaSpExpAsync(db, 500, 550));
+            ProfilingQueries(() => GetCustomersViaSpRawAsync(db, 500, 550));
+
+            ProfilingUpdateManyAsync(db, 500, 550);
+            Console.WriteLine();
         }
 
         private static void ProfilingUpdateMany(ISisoDatabase database, int customerNoFrom, int customerNoTo)
@@ -84,6 +125,28 @@ namespace SisoDb.SampleApp
             }
         }
 
+        private static void ProfilingUpdateManyAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            using (var session = database.BeginSession())
+            {
+                session.UpdateManyAsync<Customer>(
+                    c => c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo,
+                    customer => { customer.Firstname += "Udated"; }).Wait();
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("TotalSeconds = {0}", stopWatch.Elapsed.TotalSeconds);
+
+            using (var rs = database.BeginSession())
+            {
+                var rowCount = rs.Query<Customer>().Count();
+
+                Console.WriteLine("Total rows = {0}", rowCount);
+            }
+        }
+
         private static void ProfilingInserts(ISisoDatabase database, int numOfCustomers, int numOfItterations)
         {
             var stopWatch = new Stopwatch();
@@ -93,6 +156,30 @@ namespace SisoDb.SampleApp
                 var customers = CustomerFactory.CreateCustomers(numOfCustomers);
                 stopWatch.Start();
                 InsertCustomers(customers, database);
+                stopWatch.Stop();
+
+                Console.WriteLine("TotalSeconds = {0}", stopWatch.Elapsed.TotalSeconds);
+
+                stopWatch.Reset();
+            }
+
+            using (var rs = database.BeginSession())
+            {
+                var rowCount = rs.Query<Customer>().Count();
+
+                Console.WriteLine("Total rows = {0}", rowCount);
+            }
+        }
+
+        private static void ProfilingInsertsAsync(ISisoDatabase database, int numOfCustomers, int numOfItterations)
+        {
+            var stopWatch = new Stopwatch();
+
+            for (var c = 0; c < numOfItterations; c++)
+            {
+                var customers = CustomerFactory.CreateCustomers(numOfCustomers);
+                stopWatch.Start();
+                InsertCustomersAsync(customers, database);
                 stopWatch.Stop();
 
                 Console.WriteLine("TotalSeconds = {0}", stopWatch.Elapsed.TotalSeconds);
@@ -132,11 +219,28 @@ namespace SisoDb.SampleApp
             }
         }
 
+        private static void InsertCustomersAsync(int numOfItterations, int numOfCustomers, ISisoDatabase database)
+        {
+            for (var c = 0; c < numOfItterations; c++)
+            {
+                var customers = CustomerFactory.CreateCustomers(numOfCustomers);
+                InsertCustomersAsync(customers, database);
+            }
+        }
+
         private static void InsertCustomers(IEnumerable<Customer> customers, ISisoDatabase database)
         {
             using (var unitOfWork = database.BeginSession())
             {
                 unitOfWork.InsertMany(customers);
+            }
+        }
+
+        private static void InsertCustomersAsync(IEnumerable<Customer> customers, ISisoDatabase database)
+        {
+            using (var unitOfWork = database.BeginSession())
+            {
+               unitOfWork.InsertManyAsync(customers).Wait();
             }
         }
 
@@ -148,9 +252,28 @@ namespace SisoDb.SampleApp
             }
         }
 
+        private static int GetAllCustomersAsync(ISisoDatabase database)
+        {
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().ToEnumerableAsync().Result.Count();
+            }
+        }
+
         private static int GetAllCustomersAsJson(ISisoDatabase database)
         {
-            return database.UseOnceTo().Query<Customer>().ToEnumerableOfJson().Count();
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().ToEnumerableOfJson().Count();
+            }
+        }
+
+        private static int GetAllCustomersAsJsonAsync(ISisoDatabase database)
+        {
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().ToEnumerableOfJsonAsync().Result.Count();
+            }
         }
 
         private static int GetCustomersViaIndexesTable(ISisoDatabase database, int customerNoFrom, int customerNoTo)
@@ -159,6 +282,15 @@ namespace SisoDb.SampleApp
             using (var session = database.BeginSession())
             {
                 return session.Query<Customer>().OrderBy(c => c.Lastname).Where(c => c.Score > 10 && c.IsActive && c.CustomerSince > d && c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").ToEnumerable().Count();
+            }
+        }
+
+        private static int GetCustomersViaIndexesTableAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            var d = new DateTime(2012, 1, 1);
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().OrderBy(c => c.Lastname).Where(c => c.Score > 10 && c.IsActive && c.CustomerSince > d && c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").ToEnumerableAsync().Result.Count();
             }
         }
 
@@ -187,6 +319,23 @@ namespace SisoDb.SampleApp
                 return session.Advanced.NamedQuery<Customer>(q).ToArray().Length;
             }
         }
+
+        private static int GetCustomersViaSpRawAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            var d = new DateTime(2012, 1, 1);
+            using (var session = database.BeginSession())
+            {
+                var q = new NamedQuery("CustomersViaSP");
+                q.Add(new DacParameter("p0", 10));
+                q.Add(new DacParameter("p1", true));
+                q.Add(new DacParameter("p2", d));
+                q.Add(new DacParameter("p3", customerNoFrom));
+                q.Add(new DacParameter("p4", customerNoTo));
+                q.Add(new DacParameter("p5", "The delivery street #544"));
+
+                return session.Advanced.NamedQueryAsync<Customer>(q).Result.ToArray().Length;
+            }
+        }
         
         private static int GetCustomersViaSpExp(ISisoDatabase database, int customerNoFrom, int customerNoTo)
         {
@@ -202,6 +351,21 @@ namespace SisoDb.SampleApp
                     && c.DeliveryAddress.Street == "The delivery street #544").ToArray().Length;
             }
         }
+
+        private static int GetCustomersViaSpExpAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            var d = new DateTime(2012, 1, 1);
+            using (var session = database.BeginSession())
+            {
+                return session.Advanced.NamedQueryAsync<Customer>("CustomersViaSP", c =>
+                    c.Score > 10
+                    && c.IsActive
+                    && c.CustomerSince > d
+                    && c.CustomerNo >= customerNoFrom
+                    && c.CustomerNo <= customerNoTo
+                    && c.DeliveryAddress.Street == "The delivery street #544").Result.ToArray().Length;
+            }
+        }
         
         private static int GetCustomersAsJsonViaIndexesTable(ISisoDatabase database, int customerNoFrom, int customerNoTo)
         {
@@ -209,6 +373,15 @@ namespace SisoDb.SampleApp
             using (var session = database.BeginSession())
             {
                 return session.Query<Customer>().OrderBy(c => c.Lastname).Where(c => c.Score > 10 && c.IsActive && c.CustomerSince > d && c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").ToEnumerableOfJson().Count();
+            }
+        }
+
+        private static int GetCustomersAsJsonViaIndexesTableAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            var d = new DateTime(2012, 1, 1);
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().OrderBy(c => c.Lastname).Where(c => c.Score > 10 && c.IsActive && c.CustomerSince > d && c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").ToEnumerableOfJsonAsync().Result.Count();
             }
         }
 
@@ -220,11 +393,27 @@ namespace SisoDb.SampleApp
             }
         }
 
+        private static int FirstOrDefaultAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().Where(c => c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").FirstOrDefaultAsync().Result == null ? 0 : 1;
+            }
+        }
+
         private static int SingleOrDefault(ISisoDatabase database, int customerNoFrom, int customerNoTo)
         {
             using (var session = database.BeginSession())
             {
                 return session.Query<Customer>().Where(c => c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").SingleOrDefault() == null ? 0 : 1;
+            }
+        }
+
+        private static int SingleOrDefaultAsync(ISisoDatabase database, int customerNoFrom, int customerNoTo)
+        {
+            using (var session = database.BeginSession())
+            {
+                return session.Query<Customer>().Where(c => c.CustomerNo >= customerNoFrom && c.CustomerNo <= customerNoTo && c.DeliveryAddress.Street == "The delivery street #544").SingleOrDefaultAsync().Result == null ? 0 : 1;
             }
         }
     }
